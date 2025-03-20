7919161e14412faa6bc04fc0f9033d48ba1ff848
Valentin <valentin.grimm@th-owl.de>
2025-03-20 12:33:12 +0100

    fix§

--- xai/shap_individual.py
+++ xai/shap_individual.py
@@ -13,7 +13,7 @@ class SHAPIndividual:
 
     def compute_values(self, ds):
         
-        loader = DataLoader(ds, batch_size=50, shuffle=False, num_workers=1)
+        loader = DataLoader(ds, batch_size=100, shuffle=False, num_workers=1)
         
         for batch in loader:
             background = (
@@ -23,18 +23,18 @@ class SHAPIndividual:
             )
             break
         
-        explainer = shap.DeepExplainer(self.model_predict, background)
+        loader = DataLoader(ds, batch_size=50, shuffle=True, num_workers=1)
+        
+        for batch in loader:
+            sample = (
+                batch["input_ids"],
+                batch["attention_mask"],
+                batch["tabular"]
+            )
+            break
+        
+        explainer = shap.DeepExplainer(self.model, sample)
         
         shap_values = explainer.shap_values(background)
         print(shap_values)
                
-    
-    # Define your model predict function
-    def model_predict(self, input_ids, attention_mask, tabular_features):
-        input_ids = torch.tensor(input_ids)
-        attention_mask = torch.tensor(attention_mask)
-        tabular_features = torch.tensor(tabular_features)
-
-        with torch.no_grad():
-            outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, tabular_features=tabular_features)
-        return outputs
