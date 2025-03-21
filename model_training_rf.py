e7950a902d17012e45c8577021eb06e7dff26412
Valentin <valentin.grimm@th-owl.de>
2025-03-21 16:44:00 +0100

    fix

--- model_training_rf.py
+++ model_training_rf.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     train_raw = pd.DataFrame(dataset["train"])
     
     # Split data into training and testing sets
-    X_train, y_train = train_raw['statement'].to_list(), train_raw['label'].to_list()
+    X_train, y_train = train_raw['statement'].to_numpy(), train_raw['label'].to_numpy()
     
     # Initialize and train the classifier
     classifier = RandomForestTextClassifier(n_estimators=100, random_state=42, train_text=X_train)
