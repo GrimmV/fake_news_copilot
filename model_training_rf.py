8d1ac168a718ec93f02897e6a34045920bf3c92c
Valentin <valentin.grimm@th-owl.de>
2025-03-21 16:38:09 +0100

    update model training

--- model_training_rf.py
+++ model_training_rf.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     train_raw = pd.DataFrame(dataset["train"])
     
     # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(train_raw['statement'].to_list(), train_raw['label'].to_list(), test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(train_raw['statement'].to_list(), train_raw['label'].to_list(), test_size=0, random_state=42)
     
     # Initialize and train the classifier
     classifier = RandomForestTextClassifier(n_estimators=100, random_state=42, train_text=X_train)
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     
     # Evaluate the classifier
     accuracy_train = classifier.evaluate(X_train, y_train, name = "eval_train")
-    accuracy_test = classifier.evaluate(X_test, y_test, name = "eval_test")
+    # accuracy_test = classifier.evaluate(X_test, y_test, name = "eval_test")
     print(f'Accuracy in training: {accuracy_train:.2f}')
-    print(f'Accuracy in test data: {accuracy_test:.2f}')
+    # print(f'Accuracy in test data: {accuracy_test:.2f}')
